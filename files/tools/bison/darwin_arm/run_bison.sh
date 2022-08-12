02a32281a33badb5951a738de998caba36feef1e
rexdex <rexdexpl@gmail.com>
2022-08-12 20:12:10 +0200

    MacM1 Bison tool

---
+++ files/tools/bison/darwin_arm/run_bison.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+export BISON_PKGDATADIR="${DIR}/share/bison/"
+echo Bison packages found at $BISON_PKGDATADIR ...
+echo Params: "$@"
+${DIR}/bin/bison "$@"
